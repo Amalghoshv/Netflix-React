2ecb1e65cba4d0b2890a29e7050b05d41e730cbf
Amalghoshv <amalghoshv@gmail.com>
2023-02-12 11:09:07 +0530

    Completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./Nav.css"
 import logo from '../img/logo.png'
 
 function Nav() {
+    const [show,handleShow] = useState(false)
+
+    useEffect(()=>{
+        window.addEventListener('scroll',()=>{
+            if(window.scrollY>500){
+                handleShow(true)
+            }else{
+                handleShow(false)
+            }
+        })
+    },[])
+    console.log(show);
   return (
-    <div className='nav'>
+    <div className={`nav ${show && "nav-black"}`}>
         <img className='logo'
            src={logo}
            alt='logo'
